de41b3488c612c99796b380ed3cbf0aae1204122
Viktor Richter <vrichter@posteo.de>
2017-06-09 14:06:34 +0200

    Add Causes

---
+++ src/io/Cause.h
@@ -0,0 +1,29 @@
+/********************************************************************
+**                                                                 **
+** File   : src/io/Cause.h                                         **
+** Authors: Viktor Richter                                         **
+**                                                                 **
+**                                                                 **
+** GNU LESSER GENERAL PUBLIC LICENSE                               **
+** This file may be used under the terms of the GNU Lesser General **
+** Public License version 3.0 as published by the                  **
+**                                                                 **
+** Free Software Foundation and appearing in the file LICENSE.LGPL **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the license requirements will   **
+** be met: http://www.gnu.org/licenses/lgpl-3.0.txt                **
+**                                                                 **
+********************************************************************/
+
+#pragma once
+
+#include <rsb/EventId.h>
+
+namespace pontoon {
+namespace io {
+
+typedef ::rsb::EventId Cause;
+typedef std::set<Cause> Causes;
+
+} // namespace io
+} // namespace pontoon
