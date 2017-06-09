7e0883c8465a141ddb2def28999514d61dfe725d
Viktor Richter <vrichter@posteo.de>
2017-06-09 11:00:20 +0200

    Add ScaleImageOpenCV

---
+++ src/convert/ScaleImageOpenCV.cpp
@@ -0,0 +1,43 @@
+/********************************************************************
+**                                                                 **
+** File   : src/convert/ScaleImageOpenCV.cpp                       **
+** Authors: Viktor Richter                                         **
+**                                                                 **
+**                                                                 **
+** GNU LESSER GENERAL PUBLIC LICENSE                               **
+** This file may be used under the terms of the GNU Lesser General **
+** Public License version 3.0 as published by the                  **
+**                                                                 **
+** Free Software Foundation and appearing in the file LICENSE.LGPL **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the license requirements will   **
+** be met: http://www.gnu.org/licenses/lgpl-3.0.txt                **
+**                                                                 **
+********************************************************************/
+
+#include "convert/ScaleImageOpenCV.h"
+#include "utils/Exception.h"
+#include <opencv2/imgproc.hpp>
+#include "utils/CvHelpers.h"
+
+using pontoon::convert::ScaleImageOpenCV;
+typedef ScaleImageOpenCV::ImgType ImgType;
+
+ScaleImageOpenCV::ScaleImageOpenCV(double scale_width, double scale_height)
+  : _width(scale_width), _height(scale_height) {}
+
+ImgType ScaleImageOpenCV::scale(const ImgType image) const {
+  cv::InterpolationFlags interpol = cv::INTER_LINEAR;
+  if(_width == 1. && _height == 1.){
+    return image; // scaling not needed
+  } else if (_width < 1. || _height < 1.) {
+    interpol = cv::INTER_AREA;
+  } else {
+    interpol = cv::INTER_CUBIC;
+  }
+  cv::Mat src = cv::cvarrToMat(image.get());
+  boost::shared_ptr<cv::Mat> dst(new cv::Mat());
+  cv::resize(src,*dst,cv::Size(),_width,_height);
+  return pontoon::utils::cvhelpers::asIplImagePtr(dst);
+}
+
---
+++ src/convert/ScaleImageOpenCV.h
@@ -0,0 +1,41 @@
+/********************************************************************
+**                                                                 **
+** File   : src/convert/ScaleImageOpenCV.h                         **
+** Authors: Viktor Richter                                         **
+**                                                                 **
+**                                                                 **
+** GNU LESSER GENERAL PUBLIC LICENSE                               **
+** This file may be used under the terms of the GNU Lesser General **
+** Public License version 3.0 as published by the                  **
+**                                                                 **
+** Free Software Foundation and appearing in the file LICENSE.LGPL **
+** included in the packaging of this file.  Please review the      **
+** following information to ensure the license requirements will   **
+** be met: http://www.gnu.org/licenses/lgpl-3.0.txt                **
+**                                                                 **
+********************************************************************/
+
+#pragma once
+
+#include <opencv2/core/core_c.h>
+#include <utils/Subject.h>
+
+namespace pontoon {
+namespace convert {
+
+class ScaleImageOpenCV {
+public:
+
+  using ImgType = boost::shared_ptr<IplImage>;
+
+  ScaleImageOpenCV(double scale_width, double scale_height);
+
+  ImgType scale(const ImgType image) const;
+
+private:
+  double _width;
+  double _height;
+};
+
+} // namespace extract
+} // namespace pontoon
